6bc3e359d4ad3b01091518cf985389d4211a201f
sunjx <raymooonn@gmail.com>
2024-06-24 22:41:07 +0800

    fff

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import NongDa from './pages/nongda/index.tsx'
 import './index.css'
 import 'virtual:uno.css'
 import './locale/index'; // 国际化
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NongDa />
   </React.StrictMode>,
 )
---
+++ src/pages/nongda/index.tsx
@@ -0,0 +1,10 @@
+export default function Example() {
+    return (
+      <div className='flex flex-col w-full p10'>
+        <tr>
+            <td></td>
+        </tr>
+      </div>
+    )
+  }
+  
